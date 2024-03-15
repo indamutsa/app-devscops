cddd3f2d7ebf9740b3f46f9ed2d4c15bb37d0186
indamutsa <arsichizy@gmail.com>
2024-03-15 09:57:54 +0100

    Removed low level severity for developers to easily manage this...

--- installer.sh
+++ installer.sh
@@ -1,12 +1,12 @@
-#!/bin/bash
-apt-get -y -qq update
+#!/bin/sh
+# Update the apk repository and install essential packages
+apk update
 
-apt-get -y -qq  install software-properties-common # installation of python3 and python3-pip
-add-apt-repository ppa:deadsnakes/ppa
-apt-get -y -qq update
-apt-get -y -qq install python3
-apt-get -y -qq install python3-pip # python3 and pip installed
+# Install Python3 and pip (Python3 and pip are bundled together in Alpine)
+apk add --no-cache python3 py3-pip
 
-apt-get -y -qq install git # installing git
+# Install git
+apk add --no-cache git
 
-python3 -m pip install -r requirements.txt #installing pip requirements
+# Install the pip requirements
+pip3 install -r requirements.txt
