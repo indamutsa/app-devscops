1fadc7c5062af24afa132497df7b14c83cb7bfd2
indamutsa <arsichizy@gmail.com>
2024-03-15 10:49:49 +0100

    Added docker scout to scan the images with secrets set

--- installer.sh
+++ installer.sh
@@ -1,12 +1,26 @@
-#!/bin/sh
-# Update the apk repository and install essential packages
-apk update
+#!/bin/bash
+apt-get -y -qq update
 
-# Install Python3 and pip (Python3 and pip are bundled together in Alpine)
-apk add --no-cache python3 py3-pip
+apt-get -y -qq  install software-properties-common # installation of python3 and python3-pip
+add-apt-repository ppa:deadsnakes/ppa
+apt-get -y -qq update
+apt-get -y -qq install python3
+apt-get -y -qq install python3-pip # python3 and pip installed
 
-# Install git
-apk add --no-cache git
+apt-get -y -qq install git # installing git
 
-# Install the pip requirements
-pip3 install -r requirements.txt
+python3 -m pip install -r requirements.txt #installing pip requirements
+
+
+# #!/bin/sh
+# # Update the apk repository and install essential packages
+# apk update
+
+# # Install Python3 and pip (Python3 and pip are bundled together in Alpine)
+# apk add --no-cache python3 py3-pip
+
+# # Install git
+# apk add --no-cache git
+
+# # Install the pip requirements
+# pip3 install -r requirements.txt
